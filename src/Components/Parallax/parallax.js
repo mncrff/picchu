8c555bec97042482003e4b8585c9acab120a198a
Alycia Moncrieff <v-almonc@microsoft.com>
2015-10-26 13:27:36 -0700

    Updates to remove grid classes, small rework of how heros' and banners' background colors work, and other small fixes per push request review.

--- src/Components/Parallax/parallax.js
+++ src/Components/Parallax/parallax.js
@@ -1,43 +1,39 @@
-$(document).ready(function(){
-    $.msParallax = new parallaxBackground();
+function parallaxBackground() {
 
-    function parallaxBackground() {
-
-        function parallaxObj(el) {
-            this.el = $(el);
-            this.calculate();
-        }
-
-        parallaxObj.prototype.calculate = function() {
-            this.topRatio = getRatio((win.scrollTop() + win.height()) - this.el.offset().top);
-            this.midRatio = getRatio((win.scrollTop() + win.height()) - (this.el.offset().top + this.el.outerHeight() / 2));
-            this.botRatio = getRatio((win.scrollTop() + win.height()) - (this.el.offset().top + this.el.outerHeight()));
-        }
-
-        function getRatio(val) {
-            return val / win.height() * 1;
-        }
-
-        function checkScrollPos() {
-            $.each(backgrounds, function(i, bg) {
-                this.calculate();
-
-                //if the background container is in the browser viewport
-                if (this.topRatio > -0.2 && this.botRatio < 1.2) {
-                    var topPosition = Math.round(this.midRatio * -100);
-                    this.el.css({'background-position': '50% ' + topPosition + 'px'});
-                }
-            });
-        }
-
-        var win = $(window);
-
-        var backgrounds = $('.ms-parallax-background').map(function(i, el) {
-            return new parallaxObj(el);
-        }).toArray();
-
-        $(document).scroll($.throttle(10, checkScrollPos));
-
-        $(window).resize($.debounce(400, checkScrollPos));
+    function parallaxObj(el) {
+        this.el = $(el);
+        this.calculate();
     }
-});
+
+    parallaxObj.prototype.calculate = function() {
+        this.topRatio = getRatio((win.scrollTop() + win.height()) - this.el.offset().top);
+        this.midRatio = getRatio((win.scrollTop() + win.height()) - (this.el.offset().top + this.el.outerHeight() / 2));
+        this.botRatio = getRatio((win.scrollTop() + win.height()) - (this.el.offset().top + this.el.outerHeight()));
+    }
+
+    function getRatio(val) {
+        return val / win.height() * 1;
+    }
+
+    function checkScrollPos() {
+        $.each(backgrounds, function(i, bg) {
+            this.calculate();
+
+            //if the background container is in the browser viewport
+            if (this.topRatio > -0.2 && this.botRatio < 1.2) {
+                var topPosition = Math.round(this.midRatio * -100);
+                this.el.css({'background-position': '50% ' + topPosition + 'px'});
+            }
+        });
+    }
+
+    var win = $(window);
+
+    var backgrounds = $('.ms-parallax-background').map(function(i, el) {
+        return new parallaxObj(el);
+    }).toArray();
+
+    $(document).scroll($.throttle(10, checkScrollPos));
+
+    $(window).resize($.debounce(400, checkScrollPos));
+}
